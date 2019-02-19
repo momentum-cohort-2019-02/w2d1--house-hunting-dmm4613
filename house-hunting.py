580f4936ede908636d409fe72798816d09cfdd56
Dan Miller <dmm4613@gmail.com>
2019-02-19 09:15:14 -0500

    fixed the math of the month calculator

--- house-hunting.py
+++ house-hunting.py
@@ -7,17 +7,19 @@ portion_down_payment = total_cost * 0.25
 print(portion_down_payment)
 current_savings = 0
 r = 0.04
+#this will take your yearly and turn it to monthly, and figure out how much you save of that amount each month.
 portion_saved = annual_salary / 12 * portion_saved
 print(portion_saved)
 
-
+#this will take your savings pool and multiply it by an interest rate to see your monthly return, it then adds that amount to your savings.
 current_savings = current_savings + (current_savings / 12 * r)
 print(current_savings)
 
+#here we take the monthly portion you are putting into your savings and adding it to your starting monthly amount.
 current_savings = portion_saved + current_savings
 print(current_savings)
 
-months_needed = 0
+months_needed = 1
 running = True
 #This loop will run as long as running is valued at True. Once the value of current_savings reaches or exceeds portion_down_payment the loop will end and print out the amount of months needed. 
 while running:
