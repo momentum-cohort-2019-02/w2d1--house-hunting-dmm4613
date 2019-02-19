1ea39b47112351f93ad84543cc750a37c4a8b920
Dan Miller <dmm4613@gmail.com>
2019-02-19 10:03:15 -0500

    updated file to remove initial math and keep all of it in the while loop

--- house-hunting.py
+++ house-hunting.py
@@ -7,13 +7,13 @@ if str(r) == "":
     r = 0.04
 else:
     r = float(r)
+total_cost = int(input("Enter the cost of your dream home: "))
 portion_down_payment = (input("Enter the percent of your home's cost to save as a down payment [0.25]: "))
 #if no value is entered the if statement will then run a line to set the value of portion_down_payment to the default value.
 if str(portion_down_payment) == "":
     portion_down_payment = 0.25;
 else:
     portion_down_payment = float(portion_down_payment)
-total_cost = int(input("Enter the cost of your dream home: "))
 
 down_payment = total_cost * portion_down_payment
 print(down_payment)
@@ -23,15 +23,7 @@ r = 0.04
 portion_saved = annual_salary / 12 * portion_saved
 print(portion_saved)
 
-#this will take your savings pool and multiply it by an interest rate to see your monthly return, it then adds that amount to your savings.
-current_savings = current_savings + (current_savings / 12 * r)
-print(current_savings)
-
-#here we take the monthly portion you are putting into your savings and adding it to your starting monthly amount.
-current_savings = portion_saved + current_savings
-print(current_savings)
-
-months_needed = 1
+months_needed = 0
 running = True
 #This loop will run as long as running is valued at True. Once the value of current_savings reaches or exceeds down_payment the loop will end and print out the amount of months needed. 
 while running:
@@ -41,9 +33,11 @@ while running:
         print ("It will take you ", months_needed," months to put a down payment on your house.")
         running = False
     elif current_savings < down_payment:
+        #this will take your savings pool and multiply it by an interest rate to see your monthly return, it then adds that amount to your savings.
         current_savings = current_savings + (current_savings / 12 * r)
         print(current_savings)
 
+        #here we take the monthly portion you are putting into your savings and adding it to your starting monthly amount.
         current_savings = portion_saved + current_savings
         print(current_savings)
         months_needed = months_needed + 1 #simple value incrementor. Each time the loop goes through months_needed will increase.
